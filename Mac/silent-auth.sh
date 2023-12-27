a13d2f6cdb30fd6cc53226942a2b903b63a33c42
Michael <30936296+mtan93@users.noreply.github.com>
2023-12-27 14:17:12 +0000

    Update silent-auth.sh

--- Mac/silent-auth.sh
+++ Mac/silent-auth.sh
@@ -21,7 +21,7 @@ APPNA="Tailscale"
 DIR="/Applications/$APPNA.app"
 IP1=8.8.8.8
 IP2=$(echo "$TSSERVERIP")
-DT0=$(date "+DATE: %D%nTIME: %T")
+DT0=$(date "+%D %T")
 echo "Execution Record for $DT0"
 
 # SOURCES USER INFO FOR RUNASUSER COMMAND BELOW
